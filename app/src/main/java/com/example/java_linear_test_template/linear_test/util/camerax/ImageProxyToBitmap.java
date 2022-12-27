4585115fdf1b74b472444ac48480d3857b28c037
劉達 <anz.ryutatsu@gmail.com>
2022-12-27 15:47:43 +0900

    rotate and scale

--- app/src/main/java/com/example/java_linear_test_template/linear_test/util/camerax/ImageProxyToBitmap.java
+++ app/src/main/java/com/example/java_linear_test_template/linear_test/util/camerax/ImageProxyToBitmap.java
@@ -1,6 +1,7 @@
 package com.example.java_linear_test_template.linear_test.util.camerax;
 
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 
 import androidx.camera.core.ImageProxy;
 
@@ -15,7 +16,24 @@ public class ImageProxyToBitmap extends TFilter<MCImageProxy, Bitmap> {
     @Override
     public void put(MCImageProxy value) {
         Bitmap bitmap = ImageConverter.imageProxyToBitmap(value.imageProxy);
+        bitmap = rotate(bitmap, value.imageProxy.getImageInfo().getRotationDegrees());
         callAllListener(bitmap);
         value.close();
+    }
+
+    private Bitmap rotate(final Bitmap beforeBmp, int degrees) {
+        final int w = beforeBmp.getWidth();
+        final int h = beforeBmp.getHeight();
+
+        final Matrix m = new Matrix();
+        m.setRotate(degrees);
+
+        final Matrix m2 = new Matrix();
+        m2.setScale(-2,2);
+
+        final Bitmap afterBmp = Bitmap.createBitmap(
+                Bitmap.createBitmap(beforeBmp, 0, 0, w, h, m, false),
+                                                0,0, h, w, m2, false);
+        return afterBmp;
     }
 }
