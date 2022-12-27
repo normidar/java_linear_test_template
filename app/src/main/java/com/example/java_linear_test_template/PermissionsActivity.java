b6a11c690f4c5588e77c0531be7f7775bd20eb8c
劉達 <anz.ryutatsu@gmail.com>
2022-12-27 14:34:32 +0900

    permission update

---
+++ app/src/main/java/com/example/java_linear_test_template/PermissionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.java_linear_test_template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+    }
+}
